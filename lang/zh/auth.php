58ca09130722acaa6f1f4ac139950d3c53e51ae0
Dương Tùng Anh <tunnaduong@gmail.com>
2025-08-21 01:45:38 +0700

    thêm nút chuyển ngôn ngữ ở màn hình login

---
+++ lang/zh/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed' => '这些凭据与我们的记录不匹配。',
+    'password' => '提供的密码不正确。',
+    'throttle' => '登录尝试次数过多。请在 :seconds 秒后重试。',
+    'login' => '登录',
+    'phone' => '手机号码',
+    'password_label' => '密码',
+    'remember_me' => '记住我',
+    'login_button' => '登录',
+    'login_subtitle' => '登录学习管理网站',
+    'phone_placeholder' => '请输入您的手机号码',
+    'password_placeholder' => '请输入您的密码',
+];
